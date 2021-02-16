d8c3b518afab087d55d093685d919ae9a79ad457
III <lazaroiii@outlook.com>
2021-02-15 22:25:41 -0300

    Connecting robots list to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,42 +4,40 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 import ErrorBoundry from '../components/ErrorBoundry'
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: []
-        };
-    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users }))
+        this.props.onRequestRobots();
     }
 
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
 
-        return !robots.length ? <h1 className='tc'>Loading your friends</h1> :
+        return isPending 
+        ?
+        <h1 className='tc'>Loading your friends</h1>
+        :
         (
             <React.Fragment>
                 <div className='tc'>
